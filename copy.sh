902ed690b0639c108361f1d04ac782198ffe477d
Atsushi Eno <atsushieno@gmail.com>
2018-07-24 01:20:13 +0900

    Fix FrameworkList.xml and copy destination of AndroidApiInfo.xml.

--- copy.sh
+++ copy.sh
@@ -6,4 +6,5 @@ mkdir -p $DIR
 # to consist of only one directory, which means we need ALL frameworks packaged!
 cp -R $XAMARIN_ANDROID_SOURCES/bin/Debug/$DIR/v1.0 $DIR
 cp -R $XAMARIN_ANDROID_SOURCES/bin/Debug/$DIR/v9.0 $DIR
-cp AndroidApiInfo.xml $DIR
+cp AndroidApiInfo.xml $DIR/v9.0/
+cp FrameworkList.xml $DIR/v9.0/RedistList/
