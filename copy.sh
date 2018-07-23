32192041498a8db7065f3d20a60990551a501966
Atsushi Eno <atsushieno@gmail.com>
2018-07-24 00:48:57 +0900

    package v1.0 framework too.
    It is stupid, but due to silly MSBuild limitation on TargetFrameworkRootPath
    MSBuild cannot resolve framework directories to more than one directory.

--- copy.sh
+++ copy.sh
@@ -2,5 +2,8 @@
 DIR=lib/xamarin.android/xbuild-frameworks/MonoAndroid/
 
 mkdir -p $DIR
+# It is stupid, but Microsoft.Common.targets expects TargetFrameworkRootPath
+# to consist of only one directory, which means we need ALL frameworks packaged!
+cp -R $XAMARIN_ANDROID_SOURCES/bin/Debug/$DIR/v1.0 $DIR
 cp -R $XAMARIN_ANDROID_SOURCES/bin/Debug/$DIR/v9.0 $DIR
 cp AndroidApiInfo.xml $DIR
